920392a0eec1126e7e46c9285a28dd069d6b04c4
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-19 08:37:43 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -10,8 +10,8 @@ INSERT INTO users VALUES (1, "James", "21");
 INSERT INTO users VALUES (2, "John", "36");
 INSERT INTO users VALUES(3, "Mary", "23");
 
-CREATE TABLE projects (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, product_id INTEGER);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, product_id INTEGER);
  
-INSERT INTO projects VALUES (1, 7, 1, 1);
-INSERT INTO projects VALUES (2, 14, 2, 2);
-INSERT INTO projects VALUES(3, 18, 3, 3);
+INSERT INTO pledges VALUES (1, 7, 1, 1);
+INSERT INTO pledges VALUES (2, 14, 2, 2);
+INSERT INTO pledges VALUES(3, 18, 3, 3);
