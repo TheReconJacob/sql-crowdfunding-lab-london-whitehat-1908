ca394f088f6d788c2704a65df9fd9c980664ea49
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-19 08:52:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,3 +2,7 @@ INSERT INTO projects VALUES (1, "Mr", "bleh", "£10", "2/10/18", "NEVER");
 INSERT INTO projects VALUES (2, "Mr", "bleh2", "£20", "3/8/17", "NEVER");
 INSERT INTO projects VALUES(3, "Mrs", "bleh", "£50", "5/3/14", "NEVER");
 
+INSERT INTO users VALUES (1, "James", "21");
+INSERT INTO users VALUES (2, "John", "36");
+INSERT INTO users VALUES(3, "Mary", "23");
+
