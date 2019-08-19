4579b066d7bc5ad303a778aa872c6ebb7fe56c6c
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-19 08:52:32 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO projects VALUES (1, "Mr", "bleh", "£10", "2/10/18", "NEVER");
+INSERT INTO projects VALUES (2, "Mr", "bleh2", "£20", "3/8/17", "NEVER");
+INSERT INTO projects VALUES(3, "Mrs", "bleh", "£50", "5/3/14", "NEVER");
+
