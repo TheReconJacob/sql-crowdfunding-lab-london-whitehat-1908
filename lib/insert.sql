e9248d8aa52d16a9f50500769366f38d59083170
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-19 09:00:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO projects VALUES (1, "Mr", "bleh", "£10", "2/10/18", "3/10/18");
-INSERT INTO projects VALUES (2, "Ms", "bleh1", "£20", "3/8/17", "4/8/17");
-INSERT INTO projects VALUES(3, "Mrs", "bleh2", "£50", "4/3/14", "5/3/15");
-INSERT INTO projects VALUES (1, "Mq", "bleh3", "£30", "5/10/18", "6/10/18");
-INSERT INTO projects VALUES (2, "My", "bleh4", "£40", "6/8/17", "7/8/17");
-INSERT INTO projects VALUES(3, "Mry", "bleh5", "£52", "7/3/14", "8/3/14");
-INSERT INTO projects VALUES (1, "Mrss", "bleh6", "£15", "8/10/18", "9/10/18");
-INSERT INTO projects VALUES (2, "Mrsss", "bleh7", "£22", "9/8/17", "10/8/17");
-INSERT INTO projects VALUES(3, "Mrssss", "bleh8", "£59", "10/3/14", "11/3/14");
-INSERT INTO projects VALUES (1, "Mrsssss", "bleh9", "£13", "11/10/18", "12/10/18");
+INSERT INTO projects VALUES (1, "Mr", "bleh", "£10", "2/10/18", "NEVER");
+INSERT INTO projects VALUES (2, "Mr", "bleh2", "£20", "3/8/17", "NEVER");
+INSERT INTO projects VALUES(3, "Mrs", "bleh", "£50", "5/3/14", "NEVER");
+INSERT INTO projects VALUES (4, "Mr", "bleh", "£10", "2/10/18", "NEVER");
+INSERT INTO projects VALUES (5, "Mr", "bleh2", "£20", "3/8/17", "NEVER");
+INSERT INTO projects VALUES(6, "Mrs", "bleh", "£50", "5/3/14", "NEVER");
+INSERT INTO projects VALUES (7, "Mr", "bleh", "£10", "2/10/18", "NEVER");
+INSERT INTO projects VALUES (8, "Mr", "bleh2", "£20", "3/8/17", "NEVER");
+INSERT INTO projects VALUES(9, "Mrs", "bleh", "£50", "5/3/14", "NEVER");
+INSERT INTO projects VALUES (10, "Mr", "bleh", "£10", "2/10/18", "NEVER");
 
 INSERT INTO users VALUES (1, "James", "21");
 INSERT INTO users VALUES (2, "John", "36");
