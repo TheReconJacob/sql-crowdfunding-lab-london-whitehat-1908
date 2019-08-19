9136942e8c0b34b0c6db35a387d26dda97c2c9ea
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-19 08:32:11 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal TEXT, start_date TEXT, end_date TEXT);
  
-INSERT INTO projects VALUES (1, "Mr", "bleh", "£10", "2/10/18");
-INSERT INTO projects VALUES (2, "Mr", "bleh2", "£20", "3/8/17");
-INSERT INTO projects VALUES(3, "Mrs", "bleh", "£50", "5/3/14");
+INSERT INTO projects VALUES (1, "Mr", "bleh", "£10", "2/10/18", "NEVER");
+INSERT INTO projects VALUES (2, "Mr", "bleh2", "£20", "3/8/17", "NEVER");
+INSERT INTO projects VALUES(3, "Mrs", "bleh", "£50", "5/3/14", "NEVER");
