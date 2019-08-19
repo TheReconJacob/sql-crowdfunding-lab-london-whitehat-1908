c611eefe50e569c35e477ae6a944e76b17665dca
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-19 08:34:56 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,7 +4,6 @@ INSERT INTO projects VALUES (1, "Mr", "bleh", "£10", "2/10/18", "NEVER");
 INSERT INTO projects VALUES (2, "Mr", "bleh2", "£20", "3/8/17", "NEVER");
 INSERT INTO projects VALUES(3, "Mrs", "bleh", "£50", "5/3/14", "NEVER");
 
-
 CREATE TABLE projects (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
  
 INSERT INTO projects VALUES (1, "James", "21");
