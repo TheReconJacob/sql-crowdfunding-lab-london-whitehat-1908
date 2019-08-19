bc087ab75a34d31d2cfecebb71844dc15108bcf6
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-19 08:28:38 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER);
  
-INSERT INTO groceries VALUES (1, "Apples", 2, 1);
-INSERT INTO groceries VALUES (2, "Oranges", 4, 2);
-INSERT INTO groceries VALUES(3, "Peaches", 6, 3);
+INSERT INTO projects VALUES (1, "Apples", 2, 1);
+INSERT INTO projects VALUES (2, "Oranges", 4, 2);
+INSERT INTO projects VALUES(3, "Peaches", 6, 3);
